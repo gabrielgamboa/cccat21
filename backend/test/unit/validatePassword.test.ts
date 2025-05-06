357e673b81c3ac8233aac3c8a28b698690251773
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2025-05-06 14:42:19 -0300

    refactor: move validates to validators folder

--- backend/test/unit/validatePassword.test.ts
+++ backend/test/unit/validatePassword.test.ts
@@ -1,18 +1,15 @@
-import { validatePassword } from "../../src/validatePassword";
+import { validatePassword } from "../../src/validators/validatePassword";
 
 test("Deve validar a senha", () => {
-    const password = "asdQWE123";
-    const isValid = validatePassword(password);
-    expect(isValid).toBe(true);
+  const password = "asdQWE123";
+  const isValid = validatePassword(password);
+  expect(isValid).toBe(true);
 });
 
-test.each([
-    "asd",
-    "asdqwezxc",
-    "ASDQWEZXC",
-    "asdqwe123",
-    "12345678"
-])("Não deve validar a senha", (password: string) => {
+test.each(["asd", "asdqwezxc", "ASDQWEZXC", "asdqwe123", "12345678"])(
+  "Não deve validar a senha",
+  (password: string) => {
     const isValid = validatePassword(password);
     expect(isValid).toBe(false);
-});
+  }
+);
